c393f51c18326c0e98e9c7054ca2bac9930b614d
King-Austin <nworahebuka.a@gmail.com>
2025-08-23 20:30:52 +0100

    implemented the admin login

--- middleware/api/urls.py
+++ middleware/api/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     re_path(r"^gateway/?$", SecureGateway.as_view(), name="secure-gateway"),
     re_path(r"^admin/?$", AdminDataCollectionView.as_view(), name="admin-data-collection"),
     re_path(r"^rotate-key/?$", RotateMiddlewareKeyView.as_view(), name="rotate-key"),
-    re_path(r"^register/?$", AdminLogin.as_view(), name="admin-login"),
+    re_path(r"^login/?$", AdminLogin.as_view(), name="admin-login"),
+
 
 
 ]    # Add more admin endpoints as needed   
