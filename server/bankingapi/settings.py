5f419c60a6736b254624ab78b114b57121eb7f06
King-Austin <nworahebuka.a@gmail.com>
2025-09-05 10:55:35 +0100

    Update shipped

--- server/bankingapi/settings.py
+++ server/bankingapi/settings.py
@@ -24,7 +24,6 @@ SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False').lower() in ('true', '1', 'yes')
-LOCAL_DEV = False
 
 ALLOWED_HOSTS = [
     '*'
@@ -92,27 +91,19 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Replace the DATABASES section of your settings.py with this
 
-if LOCAL_DEV:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-        'CONN_MAX_AGE': 0,  # Let Supabase pooler manage connections
-        'OPTIONS': {
-            'sslmode': os.getenv('DB_SSLMODE', 'require'),
-        },
-    }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+    'USER': os.getenv('DB_USER'),
+    'PASSWORD': os.getenv('DB_PASSWORD'),
+    'HOST': os.getenv('DB_HOST'),
+    'PORT': os.getenv('DB_PORT'),
+    'CONN_MAX_AGE': 0,  # Let Supabase pooler manage connections
+    'OPTIONS': {
+        'sslmode': os.getenv('DB_SSLMODE', 'require'),
+    },
+}
 } 
 
 
