f64a553950930a109afb4abf1520557407f89e27
{Your_Full_Name} <nworahebuka.a@gmail.com>
2025-11-09 13:53:33 +0100

    chanegs made and push upstream

--- frontend/src/config/apiConfig.js
+++ frontend/src/config/apiConfig.js
@@ -4,7 +4,7 @@
 // Base URL for middleware API - change this for different environments
 
 
-export const MIDDLEWARE_BASE_URL = 'https://middleware.securecipher.app'; // Production
+export const MIDDLEWARE_BASE_URL = 'https://goldfish-app-svr3x.ondigitalocean.app' //'https://middleware.securecipher.app'; // Production
 // export const MIDDLEWARE_BASE_URL = 'http://localhost:8000'; // For local development
 
 
